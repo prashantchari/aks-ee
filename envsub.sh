c0630c5dbdea963c47155e6e5581e07973c65d46
Prashant Thiruvengadachari <pthiruv@microsoft.com>
2024-01-23 15:50:49 -0800

    fixes

--- envsub.sh
+++ envsub.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -o allexport
-source .env
-set +o allexport
-
+# set -o allexport
+# source .env
+# set +o allexport
+# Uses exported variables on the shell
+# To export from the .env file to the shell use `export $(cat .env | xargs)`
 
 for file in *.parameters.json; do
     echo "Replacing env variables in $file"
