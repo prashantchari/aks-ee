49d1f7665063948bbb2ddfee4cb71db5e0bd565e
ivotustanic <48569279+ivotustanic@users.noreply.github.com>
2024-02-21 11:45:17 -0800

    ns (#4)
    Co-authored-by: Ivo Tustanic <ivotustanic@microsoft.com>

--- artifacts/LogonScript.ps1
+++ artifacts/LogonScript.ps1
@@ -268,7 +268,7 @@ kubectl apply -f https://raw.githubusercontent.com/prashantchari/public/main/arc
 
 # The name must be a 1-127 character string, starting with a letter and containing only 0-9, a-z, A-Z, and -.
 $uniqueSecretName = "$Env:arcClusterName-$Env:resourceGroup-$Env:subscriptionId"
-$token = kubectl get secret arc-admin-secret -o jsonpath='{.data.token}' | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))}
+$token = kubectl get secret arc-admin-secret -n kube-system -o jsonpath='{.data.token}' | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))}
 
 Write-Host "Saving token"
 az keyvault secret set -n kube-system --vault-name $Env:proxyCredentialsKeyVaultName --name $uniqueSecretName --value $token
