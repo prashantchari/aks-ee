49089e58d62e8e5a2fdab8d66f81dd5ed8d846fb
Prashant Thiruvengadachari <pthiruv@microsoft.com>
2024-03-05 16:50:55 -0800

    fix

--- artifacts/LogonScript.ps1
+++ artifacts/LogonScript.ps1
@@ -164,8 +164,15 @@ Write-Host "`n"
 
 Write-Host "Prep for AIO workload deployment" -ForegroundColor Cyan
 
+
 Write-Host "Creating admin credentials"
+# Create admin service account and write token to key vault
+# The secret name must be a 1-127 character string, starting with a letter and containing only 0-9, a-z, A-Z, and -.
 kubectl apply -f https://raw.githubusercontent.com/prashantchari/public/main/arc-admin.yaml | Write-Host
+$uniqueSecretName = "$Env:clusterName-$Env:resourceGroup-$Env:subscriptionId"
+$token = kubectl get secret arc-admin-secret -n kube-system -o jsonpath='{.data.token}' | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))}
+Write-Host "Writing token to key vault: $token" 
+az keyvault secret set --vault-name $Env:proxyCredentialsKeyVaultName --name $uniqueSecretName --value $token | Write-Host
 
 
 Write-Host "Deploy local path provisioner"
@@ -296,13 +303,6 @@ if ($env:kubernetesDistribution -eq "k8s") {
 
 # enable features
 az connectedk8s enable-features --name $Env:clusterName --resource-group $Env:resourceGroup --features cluster-connect custom-locations --custom-locations-oid 51dfe1e8-70c6-4de5-a08e-e18aff23d815 | Write-Host
-
-# Create admin service account and write token to key vault
-# The secret name must be a 1-127 character string, starting with a letter and containing only 0-9, a-z, A-Z, and -.
-$uniqueSecretName = "$Env:clusterName-$Env:resourceGroup-$Env:subscriptionId"
-$token = kubectl get secret arc-admin-secret -n kube-system -o jsonpath='{.data.token}' | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))}
-Write-Host "Writing token to key vault: $token" 
-az keyvault secret set --vault-name $Env:proxyCredentialsKeyVaultName --name $uniqueSecretName --value $token | Write-Host
 
 Stop-Transcript
 exit 0
